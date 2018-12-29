9f26fb89b2ae59cc28108d55fe45372ae61ebab2
yangzhile <yangzhile@wanttreasure.com>
2018-12-29 09:59:47 +0800

    jenkins test

---
+++ src/main/java/com/zl/bootjenkins/BootJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.zl.bootjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootJenkinsApplication.class, args);
+    }
+
+}
+
